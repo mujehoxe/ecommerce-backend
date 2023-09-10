af9987a8a773e41cf3c346ab744da508edf325b2
muje hoxe <mujehoxe@gmail.com>
2023-09-10 22:44:05 +0100

    remove forestadmin

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import * as dotenv from 'dotenv';
-import { createAgent } from '@forestadmin/agent';
-import { createSqlDataSource } from '@forestadmin/datasource-sql';
 
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
-
-  const agent = createAgent({
-    authSecret: process.env.FOREST_AUTH_SECRET,
-    envSecret: process.env.FOREST_ENV_SECRET,
-    isProduction: process.env.NODE_ENV === 'production',
-    typingsPath: './typings.ts',
-    typingsMaxDepth: 5,
-  }).addDataSource(createSqlDataSource(process.env.DATABASE_URL));
-  await agent.mountOnNestJs(app).start();
 
   const port = process.env.PORT || 3000;
 
