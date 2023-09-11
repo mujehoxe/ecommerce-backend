f41c0f0e57dee741350691bcd4f47fb3c11a68b7
muje hoxe <mujehoxe@gmail.com>
2023-09-11 11:18:20 +0100

    provide only db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,7 @@ import { FileController } from './file.controller.js';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
