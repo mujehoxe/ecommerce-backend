99b0bfcb16469ff767d1293fe3b1a49ad69e3d02
muje hoxe <mujehoxe@gmail.com>
2023-09-11 10:52:45 +0100

    db type

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,12 @@ import { FileController } from './file.controller.js';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_DATABASE || 'ecommerce',
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
