50f2c1bfcd9724bf7141f71d9981d51a6b7fedea
muje hoxe <mujehoxe@gmail.com>
2023-09-13 22:26:38 +0100

    swager checkout dto

--- src/orders/dtos/checkout.dto.ts
+++ src/orders/dtos/checkout.dto.ts
@@ -1,34 +1,55 @@
 import { IsString, IsArray, ValidateNested, IsInt, Min } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
-class ProductDto {
+class CheckoutProductDto {
+  @ApiProperty({
+    description: 'The product ID',
+    minimum: 1,
+    type: 'integer',
+  })
   @IsInt()
   @Min(1)
   productId: number;
 
+  @ApiProperty({
+    description: 'The quantity of the product',
+    minimum: 1,
+    type: 'integer',
+  })
   @IsInt()
   @Min(1)
   quantity: number;
 }
 
 export class CheckoutDto {
+  @ApiProperty()
   @IsString()
   first_name: string;
 
+  @ApiProperty()
   @IsString()
   last_name: string;
 
+  @ApiProperty()
   @IsString()
   state: string;
 
+  @ApiProperty()
   @IsString()
   phoneNumber: string;
 
+  @ApiProperty()
   @IsString()
   address: string;
 
+  @ApiProperty({
+    type: CheckoutProductDto,
+    isArray: true,
+    description: 'An array of products in the checkout',
+  })
   @IsArray()
   @ValidateNested({ each: true })
-  @Type(() => ProductDto)
-  products: ProductDto[];
+  @Type(() => CheckoutProductDto)
+  products: CheckoutProductDto[];
 }
