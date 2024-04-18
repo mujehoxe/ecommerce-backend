ab5aec747728ace94e43da0f2bbe9b44ccd4e55f
muje hoxe <mujehoxe@gmail.com>
2024-04-18 19:26:13 +0100

    Get all categories public

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -4,12 +4,14 @@ import { CategoryService } from './category.service.js';
 import { CreateCategoryDto } from './dtos/create-category.dto.js';
 import { Category } from './category.entity.js';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from '../auth/auth.guard.js';
 
 @Controller('categories')
 @ApiTags('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @Public()
   @Get()
   async getAll(): Promise<Category[]> {
     return this.categoryService.getAll();
